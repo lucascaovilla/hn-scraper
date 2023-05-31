bfac71eea98ae9bfe1f971ce5d24e7a3aacb7f2a
lucascaovilla <lucasgrisac@gmail.com>
2023-05-31 17:40:10 -0300

    Initial commit

--- hn-scraper - v1/hn_request.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-def get_data(url):
-  data = requests.get(url).json()
-  print(type(data))
-  return data
--- hn-scraper - v2/hn_request.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-def get_data(url):
-  data = requests.get(url).json()
-  return data
