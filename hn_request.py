eb39a04190e8be991f429b64f1bb900055386806
lucascaovilla <lucasgrisac@gmail.com>
2023-05-31 17:41:39 -0300

    Second version complete

--- hn_request.py
+++ hn_request.py
@@ -3,5 +3,4 @@ import json
 
 def get_data(url):
   data = requests.get(url).json()
-  print(type(data))
   return data
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from hn_request import get_data
 from flask import Flask, render_template, request, redirect
 
-
 app = Flask('Maratona News')
 
 @app.route('/')
@@ -11,13 +10,9 @@ def home():
   keyword = request.args.get('order_by')
   if keyword == 'popular' or keyword == None:
     pop_list = popular_news['hits']
-    for news in pop_list:
-      news['id_url'] = news['objectID']
     return render_template('home.html', news = pop_list)
   elif keyword == 'new':
     new_list = new_news['hits']
-    for news in new_list:
-      news['id_url'] = news['objectID']
     return render_template('home.html', news = new_list)
 
 @app.route('/index')
@@ -25,9 +20,17 @@ def index():
   news_id = request.args.get('news_id')
   news_root_url = 'http://hn.algolia.com/api/v1/items/' + news_id
   info = get_data(news_root_url)
-  comments = info['children']
-  for comment in comments:
-    comment['text'] = comment['text']
+  raw_comments = info['children']
+  comments = []
+  for comment in raw_comments:
+    if comment['text'] == None:
+      continue
+    else:
+      comment['text'] = comment['text'].replace('<p>', "")
+      comment['text'] = comment['text'].replace('</p>', "")
+      comment['text'] = comment['text'].replace('<a>', "")
+      comment['text'] = comment['text'].replace('</a>', "")
+      comments.append(comment)
   return render_template('index.html', news = info, comments = comments)
 
 
